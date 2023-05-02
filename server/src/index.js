fcd1cc1b80f61def54a39704c864fa62e709d110
johnnythehobo <razvanburloiu@gmail.com>
2023-05-02 19:18:00 +0300

    t

--- server/src/index.js
+++ server/src/index.js
@@ -16,3 +16,13 @@ app.use('/auth', userRouter)
 connectDB()
 
 app.listen(5000, ()=>{console.log("test")})
+
+if(process.env.NODE_ENV ==='production'){
+    console.log("sida")
+    const path = require('path')
+
+    app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+    })
+}
